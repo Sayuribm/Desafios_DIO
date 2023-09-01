2efef8b431d2de48778af89b5b6b8d6e3579f7cc
Sayuribm <sayuribraydminori@gmail.com>
2023-09-01 13:22:58 -0300

    Create Desafio_tempo_estimado_de_entrega.py

---
+++ Desafio_tempo_estimado_de_entrega.py
@@ -0,0 +1,10 @@
+# Desafio
+#TODO: Imprimir a saída no padrão definido no enunciado deste desafio.
+#Dica: Para simplificar a formatação, utilize o conceito de interpolação de strings.
+
+nomeRestaurante = input("Digite o nome do restaurante: ")
+tempoEstimadoEntrega = int(input("Digite quanto tempo de espera em minutos: "))
+
+
+Mensagem = f"O restaurante {nomeRestaurante} entrega em {tempoEstimadoEntrega} minutos."
+print (Mensagem)
