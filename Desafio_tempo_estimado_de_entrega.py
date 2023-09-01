5d0eb987494bf5a74296ffe32ff082df9c8e9a7e
Sayuribm <sayuribraydminori@gmail.com>
2023-09-01 13:41:37 -0300

    Update Desafio_tempo_estimado_de_entrega.py

--- Desafio_tempo_estimado_de_entrega.py
+++ Desafio_tempo_estimado_de_entrega.py
@@ -5,6 +5,5 @@
 nomeRestaurante = input("Digite o nome do restaurante: ")
 tempoEstimadoEntrega = int(input("Digite quanto tempo de espera em minutos: "))
 
-
 Mensagem = f"O restaurante {nomeRestaurante} entrega em {tempoEstimadoEntrega} minutos."
 print (Mensagem)
